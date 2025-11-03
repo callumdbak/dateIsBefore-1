5e1ce971427beea86d7d4a70467c6e98fcdf499b
callumdbak <callumdbak@gmail.com>
2025-11-03 09:16:11 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 public class Main {
-   public static void main(String[] args) {
+    public static boolean dateIsBefore(int m1, int d1, int m2, int d2) {
+        if (m1 < m2) {
+            return true;
+        }
+        if (m1 == m2) {
+            return d1 < d2;
+        }
+        return false;
+    }
 
-   }
+    public static void main(String[] args) {
+        System.out.println(dateIsBefore(6, 3, 9, 20));
+        System.out.println(dateIsBefore(10, 1, 2, 25));
+        System.out.println(dateIsBefore(8, 15, 8, 15));
+        System.out.println(dateIsBefore(8, 15, 8, 16));
+    }
 }
